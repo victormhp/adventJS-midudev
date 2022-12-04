d32a118a9c714b7a22d57be6c355176ef77ed3a0
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-04 14:02:59 -0600

    Add day02 solution

---
+++ day02/index.js
@@ -0,0 +1,5 @@
+function countHours(year, holidays) {
+    return holidays.map(element => {
+        return new Date(`${year} ${element}`).getDay()
+    }).filter((element) => element !== 6 && element !== 0).length * 2
+}
