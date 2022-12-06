15dd8c64e309b5d14148bcbd80f65669e146b7aa
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-06 00:09:44 -0600

    Add day04 solution

--- day04/index.js
+++ day04/index.js
@@ -1,10 +1,6 @@
 function fitsInOneBox(boxes) {
-    boxes.sort((a, b) => a.l - b.l)
-    
-    return boxes.every((box, i) => {
-        if (i === boxes.length - 1) return true
-        const nextBox = boxes[i + 1]
-        const check = box.l < nextBox.l && box.w < nextBox.w && box.h < nextBox.h
-        return check
+    return boxes.sort((a, b) => a.l - b.l).every((box, i) => {
+        if (i == boxes.length -1) return true
+        return box.l < boxes[i + 1].l && box.w < boxes[i + 1].w && box.h < boxes[i + 1].h
     })
 }
