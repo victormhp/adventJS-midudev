68bb3af72f0cc95eaa5041075f1de0fc15004226
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-07 00:25:44 -0600

    Update day05 solution

--- day05/index.js
+++ day05/index.js
@@ -3,14 +3,14 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
     Math.max(
       ...giftsCities
       .sort((x, y) => x - y)
-      .reduce((result, i) => (
+      .reduce((total, i) => (
             i && giftsCities.unshift(giftsCities.pop()),
             (i = giftsCities
               .slice(0, maxCities)
               .reduce((acc, curr) => (acc + curr))),
-            i <= maxGifts && result.push(i),
-            i - giftsCities[maxCities - 1] <= maxGifts && result.push(i - giftsCities[maxCities - 1]),
-            result
+            i <= maxGifts && total.push(i),
+            i - giftsCities[maxCities - 1] <= maxGifts && total.push(i - giftsCities[maxCities - 1]),
+            total
             ),
           []
         ),
