9b7d37829c312a086024e1667eec4047e4acdefc
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-13 10:57:33 -0600

    Improve day10 solution.

--- day10/index.js
+++ day10/index.js
@@ -1,14 +1,11 @@
 function checkJump(heights) {
-    const max = Math.max(...heights)
-    const before = heights.slice(0, heights.indexOf(max))
-    const after = heights.slice(heights.indexOf(max) + 1)
+    const max = heights.indexOf(Math.max(...heights))
+    const before = heights.slice(0, max)
+    const after = heights.slice(max + 1)
   
-    if (before.length && after.length) {
-        const p1 = before.every((e, i, arr) => !i || e >= arr[i-1])
-        const p2 = after.every((e, i, arr) => !i || e <= arr[i-1])
+    const isAtMiddle = ![0, heights.length -1].includes(max)
+    const isBeforeSorted = before.every((e, i, arr) => !i || e >= arr[i-1])
+    const isAfterSorteed = after.every((e, i, arr) => !i || e <= arr[i-1])
       
-        return p1 && p2
-    }
-  
-    return false
+    return isAtMiddle && isBeforeSorted && isAfterSorteed
 }
