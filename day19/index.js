8d767c8bc31b9b6e0812fa269fc24dd241db9b53
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-29 21:43:27 -0600

    Add day19 solution

--- day19/index.js
+++ day19/index.js
@@ -1,6 +1,6 @@
 function sortToys(toys, positions) {
   const min = Math.min(...positions);
-  positions.forEach((_, i, arr) => arr[i] - min);
+  positions.forEach(e => e - min);
 
   return toys.sort(
     (a, b) => positions[toys.indexOf(a)] - positions[toys.indexOf(b)]
