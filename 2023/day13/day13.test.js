ab18cc597f6a15f773a5af66cbda25abec2bae9a
victormhp <victoralbertomhp@gmail.com>
2024-06-18 10:29:09 -0600

    Format

--- 2023/day13/day13.test.js
+++ 2023/day13/day13.test.js
@@ -5,10 +5,7 @@ describe('Reto #13: Calculando el Tiempo', () => {
   const testCases = [
     createTestCase([['00:10:00', '01:00:00', '03:30:00']], '-02:20:00'),
     createTestCase([['02:00:00', '05:00:00', '00:30:00']], '00:30:00'),
-    createTestCase([['00:45:00',
-  '00:45:00',
-  '00:00:30',
-  '00:00:30']], '-05:29:00'),
+    createTestCase([['00:45:00', '00:45:00', '00:00:30', '00:00:30']], '-05:29:00'),
   ];
 
   it('Type Test - should return a string', () => {
